0984499f404a969a5d9c1335afadebce09d277af
Olya Vidzi <olyaVidzi97@gmail.com>
2025-08-15 10:39:37 +0300

    first commit

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
+const email = 'test_email@gmail.com';
+const password = 'test2025';
+const username = 'test_username';
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/');
 
+    cy.get('h1').should('contain.text', 'conduit');
+    cy.get('.nav-item').contains('Sign in').click();
+
+    cy.get('h1').should('contain.text', 'Sign in');
+    cy.get('[placeholder=Email]').type(email);
+    cy.get('[placeholder=Password]').type(password);
+    cy.get('button').contains('Sign in').click();
+
+    cy.get('.nav-link').contains(username).should('be.visible');
   });
 });
